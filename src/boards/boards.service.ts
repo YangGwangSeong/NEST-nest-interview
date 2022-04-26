16a3e79325439553c65be81423ad015289f0cafc
YangGwangSeong <rhkdtjd_12@naver.com>
2022-04-26 07:58:05 -0700

    Feat : model 생성, 게시물 생성 메소드 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
-    private boards = [{"이름":"test","나이":7}]; //private 사용하는 이유는 클래스 내에서만 접근해서 수정 가능하게 하려고.
+    private boards: Board[] = []; //private 사용하는 이유는 클래스 내에서만 접근해서 수정 가능하게 하려고.
 
-    getAllBoards(){
+    getAllBoards(): Board[]{ //리턴값 타입지정
         return this.boards;
+    }
+
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id : uuid(),
+            title : title,
+            description: description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
     }
 }
