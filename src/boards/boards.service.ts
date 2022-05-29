bb57d1306a5fac0d9ca2d79be7fb4d33c9bd9933
YangGwangSeong <rhkdtjd_12@naver.com>
2022-05-29 00:29:52 -0700

    Feat : db연동 아이디를 이용해서 특정 게시물 가져오기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,40 +1,56 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
+
 @Injectable()
 export class BoardsService {
-    private boards: Board[] = []; //private 사용하는 이유는 클래스 내에서만 접근해서 수정 가능하게 하려고.
+    constructor(
+        private boardRepository: BoardRepository,
+    ){}
 
-    getAllBoards(): Board[]{ //리턴값 타입지정
-        return this.boards;
-    }
-    createBoard(createBoardDto: CreateBoardDto) {
-        const { title, description } = createBoardDto;
-        const board: Board = {
-            id : uuid(),
-            title : title,
-            description: description,
-            status: BoardStatus.PUBLIC
-        }
+    async getBoardById(id: number): Promise <Board> {
+        const found = await this.boardRepository.findOne(id);
 
-        this.boards.push(board);
-        return board;
-    }
-    getBoardById(id: string): Board {
-        const found = this.boards.find((board) => board.id === id);
         if(!found){
             throw new NotFoundException(`Can't find Board with id ${id}`);
         }
+
         return found;
     }
-    deleteBoard(id: string): void{
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter((board) => board.id !== found.id)
-    }
-    updateBoardStatus(id: string, status: BoardStatus): Board {
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
+    // private boards: Board[] = []; //private 사용하는 이유는 클래스 내에서만 접근해서 수정 가능하게 하려고.
+
+    // getAllBoards(): Board[]{ //리턴값 타입지정
+    //     return this.boards;
+    // }
+    // createBoard(createBoardDto: CreateBoardDto) {
+    //     const { title, description } = createBoardDto;
+    //     const board: Board = {
+    //         id : uuid(),
+    //         title : title,
+    //         description: description,
+    //         status: BoardStatus.PUBLIC
+    //     }
+
+    //     this.boards.push(board);
+    //     return board;
+    // }
+    // getBoardById(id: string): Board {
+    //     const found = this.boards.find((board) => board.id === id);
+    //     if(!found){
+    //         throw new NotFoundException(`Can't find Board with id ${id}`);
+    //     }
+    //     return found;
+    // }
+    // deleteBoard(id: string): void{
+    //     const found = this.getBoardById(id);
+    //     this.boards = this.boards.filter((board) => board.id !== found.id)
+    // }
+    // updateBoardStatus(id: string, status: BoardStatus): Board {
+    //     const board = this.getBoardById(id);
+    //     board.status = status;
+    //     return board;
+    // }
 }
