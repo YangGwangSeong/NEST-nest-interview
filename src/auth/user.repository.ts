41ba7005bca839f0fcba8295b16fa850a1a23e53
YangGwangSeong <rhkdtjd_12@naver.com>
2022-06-03 10:19:40 -0700

    Feat : username Unique

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
@@ -10,6 +11,16 @@ export class UserRepository extends Repository<User> {
             username : username,
             password : password
         });
-        await this.save(user);
+        try {
+
+            await this.save(user);
+            
+        } catch (error) {
+            if(error.code === '23505'){
+                throw new ConflictException('Existing username');
+            }else{
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
