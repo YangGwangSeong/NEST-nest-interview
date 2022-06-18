f93937231faeb679d262b0ad4b67701957cb1896
YangGwangSeong <rhkdtjd_12@naver.com>
2022-06-18 07:46:59 -0700

    Feat : 자신이 생성한 게시물을 삭제하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -39,8 +39,8 @@ export class BoardsService {
 
         return found;
     }
-    async deleteBoard(id:number): Promise<void> {
-        const result = await this.boardRepository.delete(id);
+    async deleteBoard(id:number, user: User): Promise<void> {
+        const result = await this.boardRepository.delete({id, user});
         
         //affected가 0이면 존재하는 아이템이 없을때
         if(result.affected ===0) {
